a9a566aa90bd158edb1bbe15ba4613ca5c1826e3
ashutosh <ashutosh.singh@aidentyx.com>
2025-07-15 16:13:17 +0530

    updated code

--- src/components/custome-node-red/property-panel/nodeFields/actionApp/root_cause_analysis/root_cause_analyst_setting.jsx
+++ src/components/custome-node-red/property-panel/nodeFields/actionApp/root_cause_analysis/root_cause_analyst_setting.jsx
@@ -37,84 +37,84 @@ const DescriptionLabel = styled.div`
 `;
 
 const RootCauseAnalystSettings = React.memo(() => {
-  const [authChecked, setAuthChecked] = useState(true);
+    const [authChecked, setAuthChecked] = useState(true);
 
-  return (
-    <>
-      <FieldRow>
-        <Label>Name</Label>
-        <Form.Item
-          name="name"
-          style={{ flex: 1, marginBottom: 0 }}
-        >
-          <StyledInput value="Root Cause Analysis"/>
-        </Form.Item>
-      </FieldRow>
+    return (
+        <>
+            <FieldRow>
+                <Label>Name</Label>
+                <Form.Item
+                    name="name"
+                    style={{ flex: 1, marginBottom: 0 }}
+                >
+                    <StyledInput value="Root Cause Analysis" />
+                </Form.Item>
+            </FieldRow>
 
-      <FieldRow>
-        <Label>URL</Label>
-        <Form.Item
-          name="url"
-          style={{ flex: 1, marginBottom: 0 }}
-          rules={[{ required: true, message: 'Please enter a URL' }]}
-        >
-          <StyledInput placeholder="Enter URL" />
-        </Form.Item>
-      </FieldRow>
+            <FieldRow>
+                <Label>URL</Label>
+                <Form.Item
+                    name="url"
+                    style={{ flex: 1, marginBottom: 0 }}
+                    rules={[{ required: true, message: 'Please enter a URL' }]}
+                >
+                    <StyledInput placeholder="Enter URL" />
+                </Form.Item>
+            </FieldRow>
 
-      <FieldRow>
-        <Label>Aanlysis Methods</Label>
-        <Form.Item
-          name="method"
-          style={{ flex: 1, marginBottom: 0 }}
-          rules={[{ required: true, message: 'Please select a method' }]}
-        >
-          <Select placeholder="Select Method">
-            <Option value="Method A">Method A</Option>
-            <Option value="Method B">Method B</Option>
-            <Option value="Method C">Method C</Option>
-          </Select>
-        </Form.Item>
-      </FieldRow>
+            <FieldRow>
+                <Label>Aanlysis Methods</Label>
+                <Form.Item
+                    name="method"
+                    style={{ flex: 1, marginBottom: 0 }}
+                    rules={[{ required: true, message: 'Please select a method' }]}
+                >
+                    <Select placeholder="Select Method">
+                        <Option value="Method A">Method A</Option>
+                        <Option value="Method B">Method B</Option>
+                        <Option value="Method C">Method C</Option>
+                    </Select>
+                </Form.Item>
+            </FieldRow>
 
-      <FieldRow>
-        <Form.Item
-          name="useAuth"
-          valuePropName="checked"
-          initialValue={true}
-          style={{ marginBottom: 0 }}
-        >
-          <Checkbox
-            style={{ color: '#333' }}
-            checked={authChecked}
-            onChange={(e) => setAuthChecked(e.target.checked)}
-          >
-            Use authentication
-          </Checkbox>
-        </Form.Item>
-      </FieldRow>
+            <FieldRow>
+                <Form.Item
+                    name="useAuth"
+                    valuePropName="checked"
+                    initialValue={true}
+                    style={{ marginBottom: 0 }}
+                >
+                    <Checkbox
+                        style={{ color: '#333' }}
+                        checked={authChecked}
+                        onChange={(e) => setAuthChecked(e.target.checked)}
+                    >
+                        Use authentication
+                    </Checkbox>
+                </Form.Item>
+            </FieldRow>
 
-      {authChecked && (
-        <FieldRow>
-          <Label>Secret</Label>
-          <Form.Item
-            name="secret"
-            style={{ flex: 1, marginBottom: 0 }}
-            rules={[{ required: true, message: 'Please enter a secret' }]}
-          >
-            <StyledInput placeholder="Enter secret" />
-          </Form.Item>
-        </FieldRow>
-      )}
+            {authChecked && (
+                <FieldRow>
+                    <Label>Secret</Label>
+                    <Form.Item
+                        name="secret"
+                        style={{ flex: 1, marginBottom: 0 }}
+                        rules={[{ required: true, message: 'Please enter a secret' }]}
+                    >
+                        <StyledInput placeholder="Enter secret" />
+                    </Form.Item>
+                </FieldRow>
+            )}
 
-      <div style={{ marginTop: 12 }}>
-        <DescriptionLabel>Description:</DescriptionLabel>
-        <Form.Item name="description" style={{ marginBottom: 0 }}>
-          <StyledTextArea rows={4} placeholder="Enter description" />
-        </Form.Item>
-      </div>
-    </>
-  );
+            <div style={{ marginTop: 12 }}>
+                <DescriptionLabel>Description:</DescriptionLabel>
+                <Form.Item name="description" style={{ marginBottom: 0 }}>
+                    <StyledTextArea rows={4} placeholder="Enter description" />
+                </Form.Item>
+            </div>
+        </>
+    );
 });
 
-export default RootCauseAnalystSettings;
+export default RootCauseAnalystSettings
